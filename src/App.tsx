b5bee9569521244c233daca424e91afacc80db5a
amirhh00 <amirhh00@gmail.com>
2023-03-23 11:34:26 +0330

    remove <Pathtracer>

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,15 @@ function App() {
   return (
     <div className="App">
       <Canvas id="three" style={{ height: '100vh' }}>
-        <Pathtracer>
-          <OrbitControls
-            makeDefault
-            target={[0, 0, 0]}
-            maxPolarAngle={Math.PI * 0.5}
-          />
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Environment preset="city" />
-          <Grass />
-        </Pathtracer>
+        <OrbitControls
+          makeDefault
+          target={[0, 0, 0]}
+          maxPolarAngle={Math.PI * 0.5}
+        />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Environment preset="city" />
+        <Grass />
       </Canvas>
     </div>
   );
